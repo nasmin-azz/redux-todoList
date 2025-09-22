bc45c1d604edbd228d7ba0d400abe3b3cc308571
nasmin-azz <nastaran.atlas@gmail.com>
2025-09-22 16:31:43 +0330

    deubug commit

--- todo/src/todoReducer.js
+++ todo/src/todoReducer.js
@@ -1,30 +1,23 @@
 import { v4 as uuidv4 } from "uuid";
 
-let initialState = {
+const initialState = {
   todos: JSON.parse(localStorage.getItem("todos")) || [],
 };
 
-export const todoReducer = (state = initialState, action) => {
+export default function todoReducer(state = initialState, action) {
   switch (action.type) {
-    case "REMOVE_TODO":
-      let updatedTodos = state.todos.filter(
-        (todo) => todo.id !== action.payload
-      );
+    case "REMOVE_TODO": {
+      const updatedTodos = state.todos.filter((todo) => todo.id !== action.payload);
       localStorage.setItem("todos", JSON.stringify(updatedTodos));
-      return {
-        ...state,
-        todos: updatedTodos,
-      };
-
-    case "ADD_TODO":
-      let newTodo = { id: uuidv4(), text: action.payload };
-      let newTodosList = [...state.todos, newTodo];
+      return { ...state, todos: updatedTodos };
+    }
+    case "ADD_TODO": {
+      const newTodo = { id: uuidv4(), text: action.payload };
+      const newTodosList = [...state.todos, newTodo];
       localStorage.setItem("todos", JSON.stringify(newTodosList));
-      return {
-        ...state,
-        todos: newTodosList,
-      };
+      return { ...state, todos: newTodosList };
+    }
     default:
       return state;
   }
-};
+}
